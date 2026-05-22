315058b316ba3bdd75f2eada9e5f36fe18022196
Aidan Liddy <aidan@aidanliddy.com>
2026-05-22 19:53:44 -0400

    Added 2026 Summer Workshops

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,122 +1,250 @@
 <template>
-	<v-layout>
-		<v-app-bar color="#1e3a8a" class="elevation-12" style="user-select: none; position: fixed">
-			<router-link to="/" class="d-flex" style="text-decoration: none; align-items: center">
-				<img alt="Team logo" class="ml-4 mr-2" src="~/assets/logo.webp" style="width: 50px" />
-				<v-app-bar-title style="width: 100%">
-					<p class="text-overline mt-n1" style="margin-bottom: -9px">FRC Team 2641</p>
-					<h5 style="margin-left: -1px" class="hidden-sm-and-down text-h5">Pittsburgh Central Catholic Robotics</h5>
-					<h5 style="margin-left: -1px" class="hidden-md-and-up text-h5">PCCR</h5>
-				</v-app-bar-title>
-			</router-link>
+  <v-layout>
+    <v-app-bar
+      color="#1e3a8a"
+      class="elevation-12"
+      style="user-select: none; position: fixed"
+    >
+      <router-link
+        to="/"
+        class="d-flex"
+        style="text-decoration: none; align-items: center"
+      >
+        <img
+          alt="Team logo"
+          class="ml-4 mr-2"
+          src="~/assets/logo.webp"
+          style="width: 50px"
+        />
+        <v-app-bar-title style="width: 100%">
+          <p class="text-overline mt-n1" style="margin-bottom: -9px">
+            FRC Team 2641
+          </p>
+          <h5 style="margin-left: -1px" class="hidden-xs text-h5">
+            Pittsburgh Central Catholic Robotics
+          </h5>
+          <h5 style="margin-left: -1px" class="hidden-sm-and-up text-h5">
+            PCCR
+          </h5>
+        </v-app-bar-title>
+      </router-link>
 
-			<v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-			<div class="hidden-md-and-down">
-				<v-btn link to="/" text="Home"></v-btn>
-				<v-btn link to="/about" text="About"></v-btn>
-				<v-btn link to="/sponsors" text="Sponsors"></v-btn>
-				<v-btn link to="/contact" text="Contact"></v-btn>
-				<v-btn class="mr-2" href="https://www.frclookout.com" target="_blank" text="Lookout" append-icon="mdi-arrow-right"></v-btn>
-			</div>
+      <div class="hidden-md-and-down">
+        <v-btn link to="/" text="Home"></v-btn>
+        <v-btn link to="/about" text="About"></v-btn>
+        <v-btn link to="/sponsors" text="Sponsors"></v-btn>
+        <v-btn link to="/contact" text="Contact"></v-btn>
+        <v-btn
+          class="mr-2"
+          href="https://www.frclookout.com"
+          target="_blank"
+          text="Lookout"
+          append-icon="mdi-arrow-right"
+        ></v-btn>
+      </div>
 
-			<v-btn class="hidden-lg-and-up mr-2" icon="mdi-menu" variant="text" @click="drawer = !drawer"></v-btn>
-		</v-app-bar>
+      <v-btn
+        class="hidden-lg-and-up mr-2"
+        icon="mdi-menu"
+        variant="text"
+        @click="drawer = !drawer"
+      ></v-btn>
+    </v-app-bar>
 
-		<v-navigation-drawer v-model="drawer" location="right" temporary class="hidden-md-and-up" style="position: fixed">
-			<v-list density="compact">
-				<v-list-item title="Home" link to="/"></v-list-item>
-				<v-list-item title="About" link to="/about"></v-list-item>
-				<v-list-item title="Sponsors" link to="/sponsors"></v-list-item>
-				<v-list-item title="Contact" link to="/contact"></v-list-item>
-				<v-list-item title="Lookout for FRC" append-icon="mdi-arrow-right" href="https://www.frclookout.com" target="_blank"></v-list-item>
-			</v-list>
-		</v-navigation-drawer>
+    <v-navigation-drawer
+      v-model="drawer"
+      location="right"
+      temporary
+      class="hidden-md-and-up"
+      style="position: fixed"
+    >
+      <v-list density="compact">
+        <v-list-item title="Home" link to="/"></v-list-item>
+        <v-list-item title="About" link to="/about"></v-list-item>
+        <v-list-item title="Sponsors" link to="/sponsors"></v-list-item>
+        <v-list-item title="Contact" link to="/contact"></v-list-item>
+        <v-list-item
+          title="Lookout for FRC"
+          append-icon="mdi-arrow-right"
+          href="https://www.frclookout.com"
+          target="_blank"
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-		<v-main>
-			<NuxtPage
-				style="
-					position: relative;
-					z-index: 1;
-					box-shadow: rgba(0, 0, 0, 0.2) 0px 7px 8px -4px, rgba(0, 0, 0, 0.14) 0px 12px 17px 2px, rgba(0, 0, 0, 0.12) 0px 5px 22px 4px;
-				"
-			/>
+    <v-main>
+      <NuxtPage
+        style="
+          position: relative;
+          z-index: 1;
+          box-shadow:
+            rgba(0, 0, 0, 0.2) 0px 7px 8px -4px,
+            rgba(0, 0, 0, 0.14) 0px 12px 17px 2px,
+            rgba(0, 0, 0, 0.12) 0px 5px 22px 4px;
+        "
+      />
 
-			<v-footer
-				:style="`display: ${mdAndUp ? 'flex' : 'block'}; padding-left: ${mdAndUp ? '15dvw' : '20px'}`"
-				style="align-items: start; z-index: 0; position: relative; overflow: hidden"
-				class="py-5"
-			>
-				<div class="mr-6">
-					<img class="hidden-sm-and-down" style="opacity: 0.9" src="@/assets/seal.webp" height="175px" />
-					<img class="hidden-md-and-up mt-n2 mb-n1" style="opacity: 0.9; margin-left: -16px" src="@/assets/seal.webp" height="175px" />
-					<div class="d-flex mx-n2 mb-2" :style="`justify-content: ${mdAndUp ? 'center' : ''}`">
-						<v-btn href="mailto:cchsteam2641@gmail.com" icon variant="text" color="grey" size="x-small">
-							<v-icon :size="20">mdi-email</v-icon>
-						</v-btn>
-						<v-btn target="_blank" href="https://www.instagram.com/frc2641" icon variant="text" color="grey" size="x-small">
-							<v-icon :size="20">mdi-instagram</v-icon>
-						</v-btn>
-						<v-btn target="_blank" href="https://www.facebook.com/Team2641Pccr" icon variant="text" color="grey" size="x-small">
-							<v-icon :size="20">mdi-facebook</v-icon>
-						</v-btn>
-						<v-btn target="_blank" href="https://twitter.com/FRC2641" icon variant="text" color="grey" size="x-small">
-							<v-icon :size="20">mdi-twitter</v-icon>
-						</v-btn>
-						<v-btn target="_blank" href="https://github.com/frc2641" icon variant="text" color="grey" size="x-small">
-							<v-icon :size="20">mdi-github</v-icon>
-						</v-btn>
-					</div>
-				</div>
+      <v-footer
+        :style="`display: ${mdAndUp ? 'flex' : 'block'}; padding-left: ${mdAndUp ? '15dvw' : '20px'}`"
+        style="
+          align-items: start;
+          z-index: 0;
+          position: relative;
+          overflow: hidden;
+        "
+        class="py-5"
+      >
+        <div class="mr-6">
+          <img
+            class="hidden-sm-and-down"
+            style="opacity: 0.9"
+            src="@/assets/seal.webp"
+            height="175px"
+          />
+          <img
+            class="hidden-md-and-up mt-n2 mb-n1"
+            style="opacity: 0.9; margin-left: -16px"
+            src="@/assets/seal.webp"
+            height="175px"
+          />
+          <div
+            class="d-flex mx-n2 mb-2"
+            :style="`justify-content: ${mdAndUp ? 'center' : ''}`"
+          >
+            <v-btn
+              href="mailto:cchsteam2641@gmail.com"
+              icon
+              variant="text"
+              color="grey"
+              size="x-small"
+            >
+              <v-icon :size="20">mdi-email</v-icon>
+            </v-btn>
+            <v-btn
+              target="_blank"
+              href="https://www.instagram.com/frc2641"
+              icon
+              variant="text"
+              color="grey"
+              size="x-small"
+            >
+              <v-icon :size="20">mdi-instagram</v-icon>
+            </v-btn>
+            <v-btn
+              target="_blank"
+              href="https://www.facebook.com/Team2641Pccr"
+              icon
+              variant="text"
+              color="grey"
+              size="x-small"
+            >
+              <v-icon :size="20">mdi-facebook</v-icon>
+            </v-btn>
+            <v-btn
+              target="_blank"
+              href="https://twitter.com/FRC2641"
+              icon
+              variant="text"
+              color="grey"
+              size="x-small"
+            >
+              <v-icon :size="20">mdi-twitter</v-icon>
+            </v-btn>
+            <v-btn
+              target="_blank"
+              href="https://github.com/frc2641"
+              icon
+              variant="text"
+              color="grey"
+              size="x-small"
+            >
+              <v-icon :size="20">mdi-github</v-icon>
+            </v-btn>
+          </div>
+        </div>
 
-				<div>
-					<p class="text-overline" style="margin-bottom: -5px">FRC Team 2641</p>
-					<h6 class="text-h6 mb-2" style="line-height: 28px">Pittsburgh Central&nbsp;<br class="hidden-md-and-up" />Catholic Robotics</h6>
-					<p class="text-body-1 mb-3 font-italic">Pro Deo et Patria</p>
-					<p class="text-body-2">Central Catholic High School</p>
-					<p class="text-body-2">4720 Fifth Ave</p>
-					<p class="text-body-2">Pittsburgh, PA 15213</p>
-					<p class="text-body-2"><a href="https://www.centralcatholichs.com" target="_blank">www.centralcatholichs.com</a></p>
-					<p class="text-body-2 mb-3"><a href="tel:4122083400">(412) 208-3400</a></p>
-					<p class="text-body-2 mb-2 transparent-subtitle">&copy; {{ new Date().getFullYear() }} Pittsburgh Central Catholic Robotics</p>
-					<p class="text-overline transparent-subtitle mt-n1 mb-n2" style="font-size: 9px !important">
-						Powered by <a href="https://www.frclookout.com" target="_blank">Lookout</a>. Design by
-						<a href="https://www.aidanliddy.com" target="_blank">Aidan Liddy</a>.
-					</p>
-				</div>
+        <div>
+          <p class="text-overline" style="margin-bottom: -5px">FRC Team 2641</p>
+          <h6 class="text-h6 mb-2" style="line-height: 28px">
+            Pittsburgh Central&nbsp;<br class="hidden-md-and-up" />Catholic
+            Robotics
+          </h6>
+          <p class="text-body-1 mb-3 font-italic">Pro Deo et Patria</p>
+          <p class="text-body-2">Central Catholic High School</p>
+          <p class="text-body-2">4720 Fifth Ave</p>
+          <p class="text-body-2">Pittsburgh, PA 15213</p>
+          <p class="text-body-2">
+            <a href="https://www.centralcatholichs.com" target="_blank"
+              >www.centralcatholichs.com</a
+            >
+          </p>
+          <p class="text-body-2 mb-3">
+            <a href="tel:4122083400">(412) 208-3400</a>
+          </p>
+          <p class="text-body-2 mb-2 transparent-subtitle">
+            &copy; {{ new Date().getFullYear() }} Pittsburgh Central Catholic
+            Robotics
+          </p>
+          <p
+            class="text-overline transparent-subtitle mt-n1 mb-n2"
+            style="font-size: 9px !important"
+          >
+            Powered by
+            <a href="https://www.frclookout.com" target="_blank">Lookout</a>.
+            Design by
+            <a href="https://www.aidanliddy.com" target="_blank">Aidan Liddy</a
+            >.
+          </p>
+        </div>
 
-				<img
-					class="hidden-sm-and-down"
-					src="@/assets/school_seal.svg"
-					style="z-index: 3; position: absolute; top: -11dvh; right: 15dvw; width: 300px; opacity: 0.2"
-				/>
-				<img
-					class="hidden-md-and-up"
-					src="@/assets/school_seal.svg"
-					style="z-index: 3; position: absolute; bottom: -5dvh; right: -5dvh; width: 250px; opacity: 0.15"
-				/>
-			</v-footer>
-		</v-main>
-	</v-layout>
+        <img
+          class="hidden-sm-and-down"
+          src="@/assets/school_seal.svg"
+          style="
+            z-index: 3;
+            position: absolute;
+            top: -11dvh;
+            right: 15dvw;
+            width: 300px;
+            opacity: 0.2;
+          "
+        />
+        <img
+          class="hidden-md-and-up"
+          src="@/assets/school_seal.svg"
+          style="
+            z-index: 3;
+            position: absolute;
+            bottom: -5dvh;
+            right: -5dvh;
+            width: 250px;
+            opacity: 0.15;
+          "
+        />
+      </v-footer>
+    </v-main>
+  </v-layout>
 </template>
 
 <script setup lang="ts">
-	const { mdAndUp } = useDisplay();
+const { mdAndUp } = useDisplay();
 
-	let drawer = ref(false);
+let drawer = ref(false);
 </script>
 
 <style>
-	.v-list-item {
-		border-radius: 12px !important;
-		margin: 0px 6px;
-	}
+.v-list-item {
+  border-radius: 12px !important;
+  margin: 0px 6px;
+}
 
-	.v-list-item__prepend > .v-badge .v-icon,
-	.v-list-item__prepend > .v-icon,
-	.v-list-item__append > .v-badge .v-icon,
-	.v-list-item__append > .v-icon,
-	.v-list-item .v-avatar {
-		margin-left: -6px;
-	}
+.v-list-item__prepend > .v-badge .v-icon,
+.v-list-item__prepend > .v-icon,
+.v-list-item__append > .v-badge .v-icon,
+.v-list-item__append > .v-icon,
+.v-list-item .v-avatar {
+  margin-left: -6px;
+}
 </style>
---
+++ pages/workshops.vue
@@ -0,0 +1,288 @@
+<template>
+  <v-row no-gutters>
+    <v-col cols="12" md="5">
+      <v-parallax
+        class="pa-0 ma-0"
+        style="height: 100%"
+        src="@/assets/2026_summer_workshop_flyer.webp"
+        cover
+      />
+    </v-col>
+
+    <v-col
+      cols="12"
+      md="7"
+      :style="lgAndUp ? 'padding: 90px' : 'padding: 24px'"
+    >
+      <h2 class="text-h2 mb-5">2026 Summer Workshops</h2>
+      <p class="subtitle">
+        Learn the essential skills for robotics: mechanical design, prototyping,
+        machining, 3D printing, electronics, programming, and more. Become
+        involved in your team!
+      </p>
+
+      <v-divider class="mt-6 mb-3"></v-divider>
+
+      <v-row>
+        <v-col cols="12" lg="8">
+          <h5 class="text-h5 mb-1">Schedule</h5>
+
+          <p class="text-body-2 mb-3 text-grey font-italic">
+            Subject to change
+          </p>
+
+          <v-card>
+            <v-table density="compact">
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Time</th>
+                  <th>Topic</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="workshop in workshops" :key="workshop.date">
+                  <td>{{ workshop.date }}</td>
+                  <td>{{ workshop.time }}</td>
+                  <td>{{ workshop.topic }}</td>
+                </tr>
+              </tbody>
+            </v-table>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12" lg="4">
+          <p class="font-weight-bold mb-2">
+            <u>Free</u> and open to all current and incoming CCHS students
+            interested in robotics.
+          </p>
+
+          <p>Lunch will <u>NOT</u> be provided, please bring a lunch.</p>
+
+          <v-divider class="my-3"></v-divider>
+          <h5 class="text-h5 mb-3">Location</h5>
+
+          <p>Central Catholic High School</p>
+          <p>STEM Building Ground Floor</p>
+          <p>4720 Fifth Ave</p>
+          <p>Pittsburgh, PA 15213</p>
+        </v-col>
+      </v-row>
+
+      <v-divider class="mt-6 mb-3"></v-divider>
+      <h5 class="text-h5 mb-3">Registration</h5>
+
+      <v-text-field
+        variant="solo"
+        hide-details
+        class="mb-4"
+        label="Parent Name"
+        v-model="data.parent_name"
+      ></v-text-field>
+
+      <v-text-field
+        variant="solo"
+        hide-details
+        class="mb-4"
+        label="Student Name"
+        v-model="data.student_name"
+      ></v-text-field>
+
+      <v-row class="pa-0 mt-0" no-gutters>
+        <v-col cols="12" sm="6">
+          <v-text-field
+            class="mr-2"
+            variant="solo"
+            hide-details
+            label="Parent Phone"
+            v-model="data.phone"
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" sm="6">
+          <v-text-field
+            variant="solo"
+            hide-details
+            class="ml-2 mb-4"
+            label="Parent Email"
+            v-model="data.email"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row class="pa-0 mt-0" no-gutters>
+        <v-col cols="12" sm="6">
+          <p class="mb-2 text-grey-lighten-1">
+            Which workshops will your son be attending?
+          </p>
+          <v-checkbox
+            color="primary"
+            density="compact"
+            hide-details
+            :label="`${workshop.date} (${workshop.topic})`"
+            v-for="workshop in workshops"
+            :key="workshop.date"
+            v-model="data.workshops[workshop.date]"
+          ></v-checkbox>
+        </v-col>
+
+        <v-col cols="12" sm="6">
+          <v-textarea
+            class="ml-2"
+            variant="solo"
+            hide-details
+            label="Additional Information"
+            v-model="data.message"
+          ></v-textarea>
+
+          <div class="d-flex mt-4" v-if="!smAndUp">
+            <v-spacer></v-spacer>
+            <v-btn
+              :disabled="
+                !data.parent_name ||
+                !data.student_name ||
+                !data.phone ||
+                !data.email
+              "
+              style="width: 125px"
+              class="elevation-24"
+              @click="send()"
+              size="large"
+              color="primary"
+              text="Send"
+              append-icon="mdi-send"
+            ></v-btn>
+          </div>
+
+          <div class="d-flex mt-4">
+            <v-spacer></v-spacer>
+            <v-btn
+              v-if="smAndUp"
+              :disabled="
+                !data.parent_name ||
+                !data.student_name ||
+                !data.phone ||
+                !data.email
+              "
+              style="width: 150px"
+              class="elevation-24"
+              @click="send()"
+              size="large"
+              color="primary"
+              text="Submit"
+              append-icon="mdi-send"
+            ></v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup lang="ts">
+const { smAndUp, lgAndUp } = useDisplay();
+
+let data: Ref<{
+  parent_name: string;
+  student_name: string;
+  phone: string;
+  email: string;
+  message: string;
+  workshops: Record<string, boolean>;
+}> = ref({
+  parent_name: "",
+  student_name: "",
+  phone: "",
+  email: "",
+  message: "",
+  workshops: {},
+});
+
+let workshops = ref([
+  {
+    date: "Sat, Jun 6",
+    time: "10 AM to 3 PM",
+    topic: "TBA",
+  },
+  {
+    date: "Sat, Jun 27",
+    time: "10 AM to 3 PM",
+    topic: "TBA",
+  },
+  {
+    date: "Sat, Jul 11",
+    time: "10 AM to 3 PM",
+    topic: "TBA",
+  },
+  {
+    date: "Sat, Jul 25",
+    time: "10 AM to 3 PM",
+    topic: "TBA",
+  },
+  {
+    date: "Sat, Aug 8",
+    time: "10 AM to 3 PM",
+    topic: "TBA",
+  },
+]);
+
+for (const workshop of workshops.value) {
+  data.value.workshops[workshop.date] = true;
+}
+
+async function send() {
+  const { parent_name, student_name, phone, email, message } = data.value;
+
+  const payload = { parent_name, student_name, phone, email, message };
+
+  try {
+    const res = await fetch("https://www.frclookout.com/api/workshops/enroll", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+    });
+
+    const json = await res.json();
+    if (!res.ok) throw new Error(json?.message || "Failed to send");
+
+    data.value = {
+      parent_name: "",
+      student_name: "",
+      phone: "",
+      email: "",
+      message: "",
+      workshops: {},
+    };
+
+    for (const workshop of workshops.value) {
+      data.value.workshops[workshop.date] = true;
+    }
+
+    alert("Message sent — thank you!");
+  } catch (err) {
+    console.error(err);
+    alert("Error sending message. Please try again later.");
+  }
+}
+
+useHead({
+  title: "Contact | Pittsburgh Central Catholic Robotics",
+});
+</script>
+
+<style scoped>
+.sticky-flyer-col {
+  position: sticky;
+  top: 0;
+  height: 100dvh;
+  align-self: flex-start;
+  padding: 0 !important;
+}
+
+.sticky-flyer-img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  display: block;
+}
+</style>
